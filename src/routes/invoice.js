abd7c133c5ed16ec1bd3bf9884f13cc0d7f18dd8
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-12-03 20:31:40 +0200

    add index route

--- src/routes/invoice.js
+++ src/routes/invoice.js
@@ -2,8 +2,8 @@ import { useParams } from "react-router-dom";
 import { getInvoice } from "../data";
 
 export default function Invoice() {
-  let params = useParams();
-  let invoice = getInvoice(parseInt(params.invoiceId, 10));
+  const params = useParams();
+  const invoice = getInvoice(parseInt(params.invoiceId, 10));
   return (
     <main style={{ padding: "1rem" }}>
       <h2>Total Due: {invoice.amount}</h2>
